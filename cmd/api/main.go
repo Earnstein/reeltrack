4b6286854361e857d180df6e2abc2384e1c00d6f
Bakare Damilola <bdamilola00@gmail.com>
2024-10-06 00:43:45 +0100

    feat: initialised server setup with httprouter

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (production | development | staging)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "INFO - ", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("Server is listening on port %s in %s mode", srv.Addr, cfg.env)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }
