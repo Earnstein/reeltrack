6f483338bea308db2d2e4c4e2f06c7f6263b1caf
Bakare Damilola <bdamilola00@gmail.com>
2024-10-10 19:29:06 +0100

    add: movie controller file

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/earnstein/reeltrack/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,5 +18,21 @@ func (app *application) getMovieHandler(w http.ResponseWriter, r *http.Request) 
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "get movie with id %d\n", id)
+
+	movie := data.Movie{
+		ID:        id,
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+		CreatedAt: time.Now(),
+	}
+	response := envelope{"movie": movie}
+
+	err = app.writeJSON(w, http.StatusOK, response, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server could not process the request", http.StatusInternalServerError)
+		return
+	}
 }
